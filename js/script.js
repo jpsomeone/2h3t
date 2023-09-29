53414e642450c80b33653d31ca7d38ccfa41eada
liviavalin <110104472+liviavalin@users.noreply.github.com>
2023-09-29 08:09:24 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
+let num1 = "";
+let num2 = "";
+let valor = "";
+let temPonto = false;
+
 function clicado(){
     alert("Tem uma mãe solteira a 300 m de vc");
 }
@@ -10,6 +15,21 @@ div = (a,b) => a / b;
 juros = (c,j) => mult(c,div(j,100));
 montante = (c,j) => soma(juros(c,j),c)
 
+mostrar_display = (msg)=>{
+    document.getElementById("resultado").value = msg;
+}
+function digitando(tecla) {
+    if(tecla == ","){
+    if(!temPonto){
+        valor += tecla;
+        mostrar_display(valor);
+        temPonto = true;
+    }
+    return;
+    }
+    valor += tecla;
+    mostrar_display(valor);
+}
 mensagem(soma(4,soma(7,soma(11,15))));
 
 mensagem(sub(18,7));
